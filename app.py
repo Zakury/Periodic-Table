6b9b48f9f4b03fc8a7cb347213e15d90522541ff
Zakury <zachary@kaczorzoo.net>
2019-11-27 12:22:53 -0500

    Added API & Data Loading in preparation for modals.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import json
 
 app = Flask(__name__)
@@ -7,5 +7,11 @@ with open("data.json") as file:
     data = json.loads(file.read())
 
 @app.route("/")
-def index():
+def periodic_table():
     return render_template("index.html", elements = data)
+
+@app.route("/api/<element_name>")
+def element(element_name):
+    for element in data:
+        if element["name"] == element_name:
+            return element
--- static/index.js
+++ static/index.js
@@ -1,5 +1,26 @@
+function display_element() {
+    let xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            // TODO: Replace with a modal.
+            console.log(xhr.responseText);
+        }
+    };
+
+    xhr.open("GET", "/api/" + window.location.hash.replace("#", ""));
+    xhr.send();
+}
+
 document.querySelectorAll(".element").forEach(element => {
     element.addEventListener("click", _ => {
         location.replace("#" + element.classList[1]);
+        display_element();
     });
+});
+
+window.addEventListener("load", _ => {
+    if (window.location.hash.startsWith("#")) {
+        display_element();
+    }
 });
