159ab19ca6cf06f6694d7428635d508058eab68f
Zakury <zachary@kaczorzoo.net>
2019-12-01 14:51:28 -0500

    Placeholder Modal code.

--- static/index.js
+++ static/index.js
@@ -3,8 +3,26 @@ function display_element() {
 
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4) {
-            // TODO: Replace with a modal.
-            console.log(xhr.responseText);
+            // Rough modal code.
+            // TODO: Make better.
+            let modal = document.querySelector(".modal");
+            let items = JSON.parse(xhr.responseText);
+            
+            modal.className = "modal open";
+            modal.innerHTML = `
+                <div class="interior">
+                    <h1>${ items["name"] }</h1>
+                    <button class="close">Close Modal.</button>
+                </div>
+            `;
+
+            let close = document.querySelector(".close")
+            close.addEventListener("click", () => {
+                modal.className = "modal closed";
+                modal.innerHTML = "";
+
+                location.replace("/");
+            });
         }
     };
 
